9c3237cefa9211fa894e8b36805da5b7561ce64e
fercamposdev <fernando.developments@gmail.com>
2024-06-11 00:40:07 -0300

    router and save marker with search params

--- src/components/AddMarker.tsx
+++ src/components/AddMarker.tsx
@@ -1,14 +1,34 @@
 import { LatLng, LeafletMouseEvent } from "leaflet";
 import { useEffect, useState } from "react";
 import { Marker, Popup, useMap } from "react-leaflet";
+import { useSearchParams } from "react-router-dom";
+
+const getPositionFromSearchParams = (searchParams: URLSearchParams): LatLng | null => {
+  if (!searchParams.get('lat') || !searchParams.get('lng')) {
+    return null;
+  }
+
+  const lat = Number(searchParams.get('lat'));
+  const lng = Number(searchParams.get('lng'));
+
+  if (Number.isNaN(lat) || Number.isNaN(lng)) return null;
+
+  return new LatLng(lat, lng);
+}
 
 const AddMarker = () => {
   const currMap = useMap();
-  const [position, setPosition] = useState<LatLng | null>(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [position, setPosition] = useState<LatLng | null>(getPositionFromSearchParams(searchParams));
 
   const onClickEvent = (e: LeafletMouseEvent) => {
-    console.log('e :>> ', e);
     setPosition(e.latlng);
+    
+    setSearchParams({
+      lat: e.latlng.lat.toString(),
+      lng: e.latlng.lng.toString(),
+    });
   }
 
   useEffect(() => {
